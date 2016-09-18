599b42a9fa38ed7c60ab8fc9b0945b462a333fec
Fabien Boco <fabien.boco@gmail.com>
2016-09-18 09:11:01 +0200

    Update Octree structure

--- src/VoxelOctree.h
+++ src/VoxelOctree.h
@@ -5,12 +5,17 @@
 class VoxelOctree
 {
 public:
+
 	VoxelOctree();
 	~VoxelOctree();
+	void setNeighbor(Side side, VoxelOctree * neighbor);
+
+	bool haveChilds();
 
 private:
 
 	VoxelOctree* m_cells[8];
+	VoxelOctree* m_neighbors[6];
 
 };
 
@@ -19,8 +24,21 @@ VoxelOctree::VoxelOctree()
 	for (int i = 0; i < 8; i++)
 		m_cells[i] = nullptr;
 	
+	for (int i = 0; i < 6; i++)
+		m_neighbors[i] = nullptr;
 }
 
 VoxelOctree::~VoxelOctree()
 {
+
+}
+
+void VoxelOctree::setNeighbor(Side side, VoxelOctree* neighbor)
+{
+	m_neighbors[side] = neighbor;
+}
+
+bool VoxelOctree::haveChilds() {
+
+	return m_cells[0] ? true : false;
 }
